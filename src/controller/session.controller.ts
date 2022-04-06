e7a713ba3f8ebb649279e0eaf57fb30a2a72d476
YohannTisserand <tisserand.yohann@gmail.com>
2022-04-06 13:44:23 +0100

    create / get sessions

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -7,27 +7,24 @@ dotenv.config();
 import config from "config";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
-  // validate user password
   const user = await validatePassword(req.body);
 
   if (!user) {
     return res.status(401).send("Invalid Email or Password");
   }
-  // create a sesssion
+
   const session = await createSession(user._id, req.get("user-agent") || "");
-  //create an access token
 
   const accessToken = signJwt(
     { ...user, session: session._id },
     { expiresIn: config.get("accessTokenTtl") }
   );
-  //create a refresh token
+
   const refreshToken = signJwt(
     { ...user, session: session._id },
     { expiresIn: config.get("accessTokenTtl") }
   );
 
-  //return access & refresh tokens
   return res.send({ accessToken, refreshToken });
 }
 
